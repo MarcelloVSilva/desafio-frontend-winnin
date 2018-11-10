40f8f6983b40c865386f01fb1a4bc92c035aef5d
MarcelloVSilva <marcellovcs@gmail.com>
2018-11-10 17:12:02 -0200

    teste reddit api

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { getHot, getNew, getRising } from '../reddit/subReddits'
+import redditInterface from '../reddit/config'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+let reddit
+beforeAll( async () => {
+  reddit = await redditInterface()
+})
+
+describe('Reddit API', async () => {
+  test('Get hot', async () => {
+    const hot = await getHot(reddit)
+    expect(hot).not.toBeNull()
+  });
+
+  test('Get news', async () => {
+    const news = await getNew(reddit)
+    expect(news).not.toBeNull()
+  });
+
+  test('get rising', async () => {
+    const rising = await getRising(reddit)
+    expect(rising).not.toBeNull()
+  });
+
+})
