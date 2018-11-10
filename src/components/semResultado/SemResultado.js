4dba2414d8f8259e40891077199ded1005029319
MarcelloVSilva <marcellovcs@gmail.com>
2018-11-10 05:12:51 -0200

    sem resultados

---
+++ src/components/semResultado/SemResultado.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SemResultado.css'
+
+class SemResultado extends React.Component {
+    render() {
+        return (
+            <p id="noresults" class="error"/>
+        )
+    }
+}
+
+export default SemResultado
