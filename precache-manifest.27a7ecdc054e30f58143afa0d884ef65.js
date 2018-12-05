810721a78284dc6154217f4da51ca16f1b89ee35
MarcelloVSilva <marcellovcs@gmail.com>
2018-12-05 17:04:17 -0200

    Updates

---
+++ precache-manifest.27a7ecdc054e30f58143afa0d884ef65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cc138d24886558ca8e6a",
+    "url": "/static/js/main.cc138d24.chunk.js"
+  },
+  {
+    "revision": "a1c3038c873d0d331e12",
+    "url": "/static/js/1.a1c3038c.chunk.js"
+  },
+  {
+    "revision": "cc138d24886558ca8e6a",
+    "url": "/static/css/main.1c993708.chunk.css"
+  },
+  {
+    "revision": "5432ba1447ca17b8045c6e4ba46efe07",
+    "url": "/index.html"
+  }
+];
